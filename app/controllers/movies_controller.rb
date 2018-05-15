356cc16e4aa831b7015a340a687868e63e5399ea
ian douglas <ian.douglas@iandouglas.com>
2018-05-15 00:16:47 -0600

    nested routes and a form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,24 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @director = Director.find(params[:director_id])
+    @movie = Movie.new
+  end
+
+  def create
+    director = Director.find(params[:director_id])
+    movie = director.movies.create(movies_params)
+    redirect_to movie_path(movie)
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  private
+
+  def movies_params
+    params.require(:movie).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   resources :movies, only: [:index]
-  resources :directors, only: [:new, :create, :index]
+
+  resources :directors, shallow: true do
+    resources :movies
+  end
+
   resources :actors, only: [:new, :create, :index]
 end
