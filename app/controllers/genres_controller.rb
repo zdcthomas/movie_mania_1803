ef53207108a70ef7838cd6c4ff85ba54655101fb
zdcthomas <zdcthomas@yahoo.com>
2018-06-05 09:20:25 -0600

    implement US 3

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,6 +5,18 @@ class GenresController < ApplicationController
   end
   
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save 
+      flash[:notice] = 'genre created'
+      redirect_to genres_path
+    else
+      flash[:error] = 'something went wrong'
+      redirect_to genres_path
+    end
+  end
+  private
 
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
