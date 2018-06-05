34e7cc5852b30a834ae4628c61856e2c48e20c5f
zdcthomas <zdcthomas@yahoo.com>
2018-06-05 09:07:58 -0600

    TEsts U S 1

---
+++ spec/features/an_admin_visits_genres_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "AnAdminVisitsGenres", type: :feature do
+  describe 'an admin' do
+    context 'visiting the genres index page' do
+      it 'should show a list of all genres' do
+        genre1 = Genre.create!(name:'Horror')
+        genre2 = Genre.create!(name:'RomCom')
+        genre3 = Genre.create!(name:'Comedy')
+        genre4 = Genre.create!(name:'SciFi')
+        admin = User.create!(username:"admin", role:1, password:'password')
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        visit genres_path
+
+        Genre.all.each do |genre|
+          expect(page).to have_content(genre.name)
+        end
+      end
+    end
+  end
+end
