45dbfb80a3a65844711b6e398750c23f00af7ac8
zdcthomas <zdcthomas@yahoo.com>
2018-06-05 09:47:54 -0600

    implements US 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180605153418) do
+ActiveRecord::Schema.define(version: 20180605154725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180605153418) do
     t.datetime "updated_at", null: false
     t.bigint "director_id"
     t.string "slug"
+    t.integer "rating"
     t.index ["director_id"], name: "index_movies_on_director_id"
   end
 
