3f9d1149be6e737ad90dd8325e032fc6321ced82
zdcthomas <zdcthomas@yahoo.com>
2018-06-05 09:17:36 -0600

    TESTS US 3

--- spec/features/an_admin_visits_genres_spec.rb
+++ spec/features/an_admin_visits_genres_spec.rb
@@ -30,6 +30,23 @@ RSpec.feature "AnAdminVisitsGenres", type: :feature do
         expect(page).to have_field('genre[name]')
         expect(page).to have_button('Create Genre')
       end
+      it 'should create a new genre after filling out the form' do
+        genre1 = Genre.create!(name:'Horror')
+        genre2 = Genre.create!(name:'RomCom')
+        genre3 = Genre.create!(name:'Comedy')
+        genre4 = Genre.create!(name:'SciFi')
+        admin = User.create!(username:"admin", role:1, password:'password')
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        visit genres_path
+
+        name = "Musical"
+        fill_in'genre[name]', with: name
+        click_on('Create Genre')
+
+        expect(current_path).to eq(genres_path)
+        expect(page).to have_content(name)
+      end
     end
   end
 end
